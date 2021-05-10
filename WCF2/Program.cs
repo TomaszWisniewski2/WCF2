f81179afa0e7fb5e23195041e6695972d54b0229
Tomasz Wiśniewski <xmen2237@wp.pl>
2021-05-10 14:07:23 +0200

    Dodaj pliki projektów.

---
+++ WCF2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF2
+{
+    class Program
+    {
+        [ServiceContract]
+        public interface IMessageService
+        {
+            [OperationContract]
+            string[] GetMessages();
+
+        }
+
+        [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+        public class MessageService : IMessageService
+        {
+            public string[] GetMessages()
+            {
+                return new string[] { "s1", "s2", "s3" };
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            var uris = new Uri[1];
+            string address = "net.tcp://localhost:6565/MessageService";
+            uris[0] = new Uri(address);
+            IMessageService message = new MessageService();
+            ServiceHost host = new ServiceHost(message,uris);
+            var binding =new NetTcpBinding(SecurityMode.None);
+            host.AddServiceEndpoint(typeof(IMessageService), binding, "");
+            host.Opened += Host_Opened;
+            host.Open();
+            Console.ReadLine();
+        }
+        private static void Host_Opened(object sender, EventArgs e)
+        {
+            Console.WriteLine("message service started");
+        }
+    }
+}
---
+++ WCFC/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFC
+{
+    [ServiceContract]
+    public interface IMessageService
+    {
+        [OperationContract]
+        string[] GetMessages();
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("press aany key to cont..");
+            Console.ReadLine();
+            string uri = "net.tcp://localhost:6565/MessageService";
+            NetTcpBinding binding = new NetTcpBinding(SecurityMode.None);
+            var channel = new ChannelFactory<IMessageService>(binding);
+            var endpoint = new EndpointAddress(uri);
+            var proxy = channel.CreateChannel(endpoint);
+            var result = proxy?.GetMessages();
+            if(result != null)
+            {
+                result.ToList().ForEach(p => {
+                    Console.WriteLine(p);
+                });
+                Console.ReadLine();
+            }
+        }
+    }
+}
